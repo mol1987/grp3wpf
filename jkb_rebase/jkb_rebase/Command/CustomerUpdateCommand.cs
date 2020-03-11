63ed675e3ab661cc170f69f5c14544a45668ed96
Jakob Andersson <jakob.aah@gmail.com>
2020-03-11 16:33:22 +0100

    Fixed error, put the project inside a folder instead, like master

---
+++ jkb_rebase/jkb_rebase/Command/CustomerUpdateCommand.cs
@@ -0,0 +1,28 @@
+﻿using jkb_rebase.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace jkb_rebase.Command
+{
+    public class CustomerUpdateCommand : ICommand
+    {
+        private CustomerViewModel _ViewModel;
+        public event EventHandler CanExecuteChanged;
+        public CustomerUpdateCommand(CustomerViewModel viewModel)
+        {
+            _ViewModel = viewModel;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _ViewModel.SaveChanges();
+        }
+    }
+}
