015efbb9e699684e02e8ed74533fbb1fb5b44396
jakob andersson <jakob.aah@gmail.com>
2020-03-20 15:23:53 +0100

    Lagt till exempel på Updatering av en ObservableCollection

---
+++ alpha/Model/Article.cs
@@ -0,0 +1,14 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace alpha
+{
+    [AddINotifyPropertyChangedInterface]
+    public class Article
+    {
+        public string Name { get; set; } = "...";
+        public double Price { get; set; } = 0.0;
+    }
+}
